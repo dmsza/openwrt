affdff23113c5615d0e3d260066eaeff1c588ab0
GLiNet-Tech <glrouter@gl-inet.com>
2026-01-27 09:39:21 +0800

    mediatek: filogic: add support for GL.iNet MT5000 (Brume3)
    This patch adds support for GL.iNet MT5000.
    
    The GL.iNet MT5000 is a gateway device equipped with three 2.5G Ethernet
    ports, two of which are LAN ports and one is a WAN port. Currently, this
    submission does not support LAN-to-LAN communication; only WAN-to-LAN
    functionality is guaranteed to operate normally.
    
    Hardware Specification:
    - SoC: MediaTek MT7987A (Quad-core ARM Cortex-A53 2.0 GHz)
    - RAM: 1024 MiB DDR4
    - Flash: 8192 MiB EMMC
    - Ethernet:
    - 2x 10/100/1000/2500 Mbps LAN (Realtek RTL8366ub Switch)
    - 1x 10/100/1000/2500 Mbps LAN (SoC internal PHY)
    - Buttons: Reset,
    - LEDs: 3x (Blue: Run, White: System, White: Vpn)
    - UART: 115200 8n1 (VCC, RX, TX, GND)
    
    Flash Layout:
    - 0x0~0x4400    : GPT
    - 0x400000~0x480000  : u-boot-env
    - 0x480000~0x880000  : Factory
    - 0x880000~0xa80000  : fip
    - 0xa80000~0xc80000  : cfg
    - 0xc80000~0xe80000  : log
    - 0xe80000~0x2e80000 : kernel
    - 0x2e80000~0x12a80000: rootfs
    
    MAC Addresses (NVMEM):
    - Base MAC located at Factory partition offset 0x4
    - gmac1 (WAN) : Base - 2 (Label MAC)
    - gmac0 (LAN) : Base - 1
    
    Installation:
    1. You can directly upgrade using the
       openwrt-mediatek-filogic-glinet_gl-mt5000-squashfs-sysupgrade.bin
       image on the official GL firmware upgrade page.
    2. Access the device and use the sysupgrade command to upgrade.Note:
       When upgrading from official firmware, use the -n parameter to
       preserve no configuration.
    
    Signed-off-by: GLiNet-Tech <glrouter@gl-inet.com>
    (cherry picked from commit 4cbd3fffd192da114f3411e1bd49c99c5bb65bce)

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -194,6 +194,7 @@ platform_do_upgrade() {
 	arcadyan,mozart|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt2500-airoha|\
+	glinet,gl-mt5000|\
 	glinet,gl-mt6000|\
 	glinet,gl-x3000|\
 	glinet,gl-xe3000|\
@@ -437,6 +438,7 @@ platform_copy_config() {
 	arcadyan,mozart|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt2500-airoha|\
+	glinet,gl-mt5000|\
 	glinet,gl-mt6000|\
 	glinet,gl-x3000|\
 	glinet,gl-xe3000|\
